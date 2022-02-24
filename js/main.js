a1715ce94e3421b8662b2ccfc7a1a0abeb92186d
SkyisHere <pav-le@mail.ru>
2022-02-24 23:38:21 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,129 @@
+// Select Html elements
+const body = document.querySelector('body');
+const header = body.querySelector('header');
+const headerWrapper = body.querySelector('.header__wrapper');
+const sections = body.querySelectorAll('.section-wrapper');
+// Burger menu elements
+const burgerMenu = body.querySelector('.burger-menu');
+const burgerBtn = burgerMenu.querySelector('.burger-menu__button');
+const links = body.querySelectorAll('.burger-menu__link');
+const overlay = burgerMenu.querySelector('.burger-menu__overlay');
+// Popup
+const popup = body.querySelector('.popup');
+
+
+// Burger menu
+burgerBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleMenu();
+});
+
+header.addEventListener('click', (event) => {
+    const link = event.target;
+    if (link.classList.contains('nav-item-js')) {
+        smoothScroll(event, link);
+        toggleMenu();
+    }
+})
+
+overlay.addEventListener('click', toggleMenu);
+
+// Functions
+function toggleMenu() {
+    burgerMenu.classList.toggle('burger-menu_active');
+    body.classList.toggle('hidden');
+}
+
+function smoothScroll(event, link) {
+    event.preventDefault();
+    const href = link.getAttribute('href') || link.dataset.href;
+    const target = document.querySelector(`${href}`);
+
+    if (target !== null) {
+        const headerHeight = burgerBtn.clientHeight || headerWrapper.clientHeight;
+        const topPosition = target.offsetTop - headerHeight;
+
+        window.scrollTo({
+            top: topPosition,
+            behavior: 'smooth'
+        });
+    }
+}
+
+// Добавляем прослушивателя для шапки, чтобы отследить
+// когда она выйдет за пределы видимости и добавляем ей класс для анимации
+const headerObserver = new IntersectionObserver((entries, observer) => {
+    if (entries[0].isIntersecting) {
+        header.classList.remove('_scroll');
+        links[0].classList.add('active');
+    } else {
+        header.classList.add('_scroll');
+    }
+}, { threshold: 1.0 });
+
+headerObserver.observe(header);
+
+// Добавляем прослушивателей для секций
+const sectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            changeActiveMenuItem(entry.target.id);
+        }
+    });
+}, { threshold: 0.7 });
+
+sections.forEach((section) => {
+    sectionObserver.observe(section);
+});
+
+function changeActiveMenuItem(sectionId) {
+    const menuItem = document.querySelector(`a[href="#${sectionId}"]`);
+
+    links.forEach((item) => item.classList.remove('active'));
+    menuItem.classList.add('active');
+}
+
+// Добавление проектов в блоки projects
+const layoutEl = body.querySelector('#layout .projects');
+const webAppsEl = body.querySelector('#web-apps .projects');
+const spaEl = body.querySelector('#spa .projects');
+
+addProjects(layoutEl, data, 'layouts', 'grow');
+addProjects(webAppsEl, data, 'jsapps', 'opacity');
+addProjects(spaEl, data, 'spa', 'translate');
+
+// Анимация для созданных выше блоков projects
+const projectEls = body.querySelectorAll('.projects__item');
+
+window.addEventListener('scroll', () => {
+    createBlockScrollAnimation(projectEls);
+});
+
+
+// Работа с модальным окном, который содержит детали проектов
+function togglePopup() {
+    popup.classList.toggle('active');
+    body.classList.toggle('hidden-popup');
+}
+
+function popupHandle(event) {
+    // event.preventDefault()
+    const target = event.target;
+
+    if (!target.closest('.popup-body-js') || target.classList.contains('popup__close')) {
+        togglePopup();
+    }
+}
+
+popup.addEventListener('click', popupHandle);
+
+body.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.closest('.details-link-js')) {
+        event.preventDefault();
+        const project = target.closest('.projects__item');
+        togglePopup();
+        addProjectDetails(project.id);
+    }
+})
---
+++ js/utils.js
@@ -0,0 +1,87 @@
+// Add projects__item to the projects section
+function addProjects(block, data, type = '', showClass = 'grow') {
+    const projectsElems = data.filter(elem => elem.type === type).map((item) => {
+        const projectEl = createProjectEl(item, showClass);
+
+        return projectEl;
+    });
+
+    block.innerHTML = projectsElems.join('');
+}
+
+// Create a project element
+function createProjectEl(item, showClass) {
+    const projectItem = `
+    <div id="${item.id}" class="projects__item ${showClass}">
+      <div class="card">
+        <a href="${item.link}" class="details-link-js" rel="noopener noreferrer" target="_blank">
+          <img src="${item.img}" alt="${item.title} image" title="Кликни, чтобы посмотреть демо-версию">
+          <h4>${item.title}</h4>
+          <p>${item.description}</p>
+        </a>
+      </div>
+    </div>
+  `;
+
+    return projectItem;
+}
+
+// Animate projects__item with scroll
+function createBlockScrollAnimation(blocks) {
+    // Получить 4/5 высоты экрана
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    blocks.forEach((block) => {
+        const blockTop = block.getBoundingClientRect().top
+
+        if (blockTop < triggerBottom) {
+            block.classList.add('show');
+        }
+    });
+}
+
+
+// Fill the popup element with a project details
+function createProjectDetails(projectData) {
+    const projectDetails = `
+    <div class="popup__header">
+      <span href="" class="popup__close">&#10008;</span>
+      <h3 class="popup__title">Приложение ${projectData.title}</h3>
+    </div>
+
+    <div class="popup__body">
+      <div class="popup__short-description">
+        <img src="${projectData.img}" alt="картинка ${projectData.title}" class="popup__img">
+        <div class="popup__text">${projectData.description}</div>
+
+      </div>
+
+      <div class="popup__long-description">${projectData.longDescription}</div>
+
+      <div class="popup__stack">
+        Стек: <span class="popup__stack-color">${projectData.stack}</span>
+      </div>
+
+      <div class="popup__project-links">
+        <a href="${projectData.link}"
+          class="popup__btn gh-pages"
+          rel="noopener noreferrer" target="_blank"
+        >Демо-версия</a>
+        <a href="${projectData.sourceLink}"
+          class="popup__btn source-code"
+          rel="noopener noreferrer" target="_blank"
+        >Код на Github</a>
+      </div>
+    </div>
+  `;
+
+    return projectDetails;
+}
+
+function addProjectDetails(projectId) {
+    const projectData = data.find(project => project.id === projectId);
+    const popupEl = document.querySelector('.popup-body-js');
+
+    const projectDetailsEl = createProjectDetails(projectData);
+    popupEl.innerHTML = projectDetailsEl;
+}
